dad2128de2f147d6f58f0032c5d3eb16b3e515d9
Benoit Martin <bmartin@cea.fr>
2025-11-13 11:42:47 +0100

    fix packages in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 # =============================================================================
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def readme():
@@ -50,7 +50,7 @@ setup(name='deisa-common',
       author_email='bmartin@cea.fr',
       python_requires='>=3.8',
       keywords='deisa in-situ',
-      packages=['deisa-common'],
+      packages=find_packages(include=['deisa', 'deisa.*']),
       classifiers=[
           "Programming Language :: Python :: 3.8",
           "Operating System :: OS Independent",
