dff8d08c996a3c30d85ce5c945894756691b5a52
Benoit Martin <bmartin@cea.fr>
2025-11-13 11:31:47 +0100

    add setup.py and readme

---
+++ setup.py
@@ -0,0 +1,63 @@
+# =============================================================================
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the names of CEA, nor the names of the contributors may be used to
+#   endorse or promote products derived from this software without specific
+#   prior written  permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(name='deisa-common',
+      version='0.1.0',
+      description='Deisa common',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      license='MIT',
+      url='https://github.com/deisa-project/common',
+      project_urls={
+          'Bug Reports': 'https://github.com/deisa-project/common/issues',
+          'Source': 'https://github.com/deisa-project/common',
+      },
+      author='Benoît Martin',
+      author_email='bmartin@cea.fr',
+      python_requires='>=3.8',
+      keywords='deisa in-situ',
+      packages=['deisa-common'],
+      install_requires=[],
+      tests_require=[],
+      test_suite='test',
+      classifiers=[
+          "Programming Language :: Python :: 3.8",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Development Status :: 3 - Alpha"
+      ]
+      )
